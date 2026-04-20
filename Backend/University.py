d9ca32a669f567df14f106e8532f6fc755e2be22
Andreas Gomes Marchi <Andreasgm38@gmail.com>
2026-04-20 16:42:40 -0300

    Feat: Incluindo o arquivo University.py

---
+++ Backend/University.py
@@ -0,0 +1,16 @@
+
+class University:
+    def __init__(self, name, city, country, climate, nationalPosition, internationalPosition, documents, type, scholarships, site):
+        self.name = name
+        self.city = city
+        self.country = country
+        self.climate = climate
+        self.nationalPosition = nationalPosition
+        self.internationalPosition = internationalPosition
+        self.documents = documents
+        self.type = type
+        self.scholarships = scholarships
+        self.site = site
+
+    def __str__(self) -> str:
+        return f"University(name={self.name}, city={self.city}, country={self.country}, climate={self.climate}, nationalPosition={self.nationalPosition}, internationalPosition={self.internationalPosition}, documents={self.documents}, type={self.type}, scholarships={self.scholarships}, site={self.site})"
